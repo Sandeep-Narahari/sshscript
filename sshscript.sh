c2d0d2df880f2fd9db482342db9c07a81fbf3910
Sandeep Narahari <46187761+Sandeep-Narahari@users.noreply.github.com>
2022-09-08 19:04:14 +0530

    Update sshscript.sh

--- sshscript.sh
+++ sshscript.sh
@@ -6,8 +6,11 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y sudo nano wget tar zip unzip jq ssh
 apt-get install -y openssh-server
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-(echo ${my_root_password}; echo ${my_root_password}) | passwd root
+#echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+mkdir ~/.ssh
+nano authorized_keys
+echo ${my_pub_key} >> authorized_keys
+#(echo ${my_pub_key}; echo ${my_pub_key}) | passwd root
 service ssh restart
 source $HOME/.bashrc
 sleep infinity
