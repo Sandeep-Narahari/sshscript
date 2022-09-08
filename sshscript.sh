bdd5390b198463d7ceff3f5ee0f3b7a52d8bdca6
Sandeep Narahari <46187761+Sandeep-Narahari@users.noreply.github.com>
2022-09-08 17:24:19 +0530

    Update timezone and installing ssh

--- sshscript.sh
+++ sshscript.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+source $HOME/.bashrc
+TZ=Asia/Kolkata
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+apt-get update
+apt-get upgrade -y
+apt-get install -y sudo nano wget tar zip unzip jq ssh
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 service ssh restart
+source $HOME/.bashrc
 sleep infinity
+
+
