9da58589070187285830a818a62adceda7d7e980
Sandeep Narahari <46187761+Sandeep-Narahari@users.noreply.github.com>
2022-09-08 17:30:55 +0530

    installing open ssh

--- sshscript.sh
+++ sshscript.sh
@@ -5,6 +5,7 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt-get update
 apt-get upgrade -y
 apt-get install -y sudo nano wget tar zip unzip jq ssh
+apt-get install -y openssh-server
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 service ssh restart
